c0b8d80c46c1560681ef39080ce8633f87e51bad
anet-eny <anet.eny@gmail.com>
2026-04-04 17:18:43 +0200

    refactor: uncomment Hero component in Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
+import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      {/* <Hero />
-   <OurScience />
-   <Products />
-   <WhyUs />
-   <MeetUs />
-   <Footer /> */}
+      <Hero />
     </>
   );
 }
